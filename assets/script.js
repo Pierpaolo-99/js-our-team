cc881906b16f3734725cee6e9018177c628efcc9
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-24 17:37:39 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+console.log(teamMembers);
+
+const colEl = document.querySelector('col')
+
+function myFunction (...arr){
+  const arrCard = []
+  const {name,role,email,img} = teamMembers
+  for (let i = 0; i < arr.length; i++){
+    thisMembersName = arr[i][name,role,email,img]
+    arrCard.push(thisMembersName)
+  }
+  return arrCard
+}
+
+const card = myFunction(teamMembers)
+console.log(card);
+
+
+
